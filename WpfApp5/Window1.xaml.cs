cabcbbe78d218a6838c2adf398edc66f8f1c4475
Ananeva Alisa <j0d2x7@gmail.com>
2022-04-04 09:28:00 +0300

    Добавьте файлы проекта.

---
+++ WpfApp5/Window1.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp5
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonAddName_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtName.Text) && !lstNames.Items.Contains(txtName.Text))
+            {
+                lstNames.Items.Add(txtSurname.Text + " " + txtName.Text + " "+ txtPatr.Text+ " " + txtNumber.Text);
+                txtName.Clear();
+            }
+        }
+    }
+}
